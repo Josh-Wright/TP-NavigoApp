1156329b7db69f5184701db73f0ccdb5ebdf7d7b
Guilherme Barnes <gulhermebarnes920@gmail.com>
2025-05-11 15:55:54 +0100

    Completed progress bar to match wireframe to depict complete and remaining parts of the journey

--- Navigo-UI/Components/BusRoutePage.js
+++ Navigo-UI/Components/BusRoutePage.js
@@ -9,19 +9,25 @@ import {
   faClock,
   faHandPointer,
   faHeadset,
+  faBus,
 } from "@fortawesome/free-solid-svg-icons";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 
 const origin = { latitude: 52.929488857183586, longitude: -1.497704357336529 };
-
 const destination = {
   latitude: 52.91902299061751,
   longitude: -1.472203979339939,
 };
-const REDACTED = "you are not getting my key";
-const waypoint1 = { latitude: 52.923025311374005, longitude: -1.4813591528924195 };
-const waypoint2 = { latitude: 52.92430774323713, longitude: -1.4809774950254628 };
+const REDACTED = "REDACTED";
+const waypoint1 = {
+  latitude: 52.923025311374005,
+  longitude: -1.4813591528924195,
+};
+const waypoint2 = {
+  latitude: 52.92430774323713,
+  longitude: -1.4809774950254628,
+};
 
 export default function BusRoutePage() {
   return (
@@ -30,30 +36,50 @@ export default function BusRoutePage() {
       <RouteSummary />
 
       <Body>
-        <ProximityNotification />
-        <NextStop />
-        <MapView
-          style={styles.mapView}
-          initialRegion={{
-            latitude: (origin.latitude + destination.latitude) / 2,
-            longitude: (origin.longitude + destination.longitude) / 2,
-            latitudeDelta: 0.015, //zoom
-            longitudeDelta: 0.015, 
-          }}
-        >
-          <Marker coordinate={origin} title="Origin" />
-          <Marker coordinate={destination} title="Destination" />
-          <MapViewDirections
-            origin={origin}
-            destination={destination}
-            apikey={REDACTED}
-            strokeWidth={3}
-            strokeColor="black"
-            waypoints={[waypoint1, waypoint2]}
-          />
-        </MapView>
+        {/* <ProximityNotification /> */}
+        <NextStop/>
+        <Destination />
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.mapView}
+            initialRegion={{
+              latitude: (origin.latitude + destination.latitude) / 2,
+              longitude: (origin.longitude + destination.longitude) / 2,
+              latitudeDelta: 0.015, //zoom
+              longitudeDelta: 0.015,
+            }}
+          >
+            <Marker coordinate={origin} title="Origin" />
+            <Marker coordinate={destination} title="Destination" />
+            <MapViewDirections
+              origin={origin}
+              destination={destination}
+              apikey={REDACTED}
+              strokeWidth={3}
+              strokeColor="black"
+              waypoints={[waypoint1, waypoint2]}
+            />
+          </MapView>
+        </View>
         <Support />
       </Body>
+    </View>
+  );
+}
+
+function NextStop() {
+  return (
+    <View style={nextStopStyles.container}>
+      <Text style={nextStopStyles.title}>Next stop: Keddleston road</Text>
+      <View style={nextStopStyles.infoRow}>
+        <FontAwesomeIcon icon={faInfoCircle} color="#0e766f" />
+        <Text style={nextStopStyles.text}>Arriving in 2 minutes</Text>
+      </View>
+      <View style={nextStopStyles.infoRow}>
+        <FontAwesomeIcon icon={faClock} color="#0e766f" />
+        <Text style={nextStopStyles.text}>This is not your stop</Text>
+      </View>
+    
     </View>
   );
 }
@@ -77,21 +103,25 @@ function ProximityNotification() {
   );
 }
 
-function NextStop() {
+function Destination() {
   return (
     <View style={nextStopStyles.container}>
-      <Text style={nextStopStyles.title}>Next stop: Keddleston road</Text>
+      <Text style={nextStopStyles.title}>Your Destination: MS125</Text>
       <View style={nextStopStyles.infoRow}>
         <FontAwesomeIcon icon={faInfoCircle} color="#0e766f" />
-        <Text style={nextStopStyles.text}>Arriving in 2 minutes</Text>
+        <Text style={nextStopStyles.text}>
+          Arriving in approximately 4 minutes
+        </Text>
       </View>
       <View style={nextStopStyles.infoRow}>
         <FontAwesomeIcon icon={faClock} color="#0e766f" />
-        <Text style={nextStopStyles.text}>This is not your stop</Text>
+        <Text style={nextStopStyles.text}>
+          You will receive alerts as you get closer
+        </Text>
       </View>
       <TouchableOpacity style={nextStopStyles.button}>
         <FontAwesomeIcon icon={faHandPointer} color="#ffffff" />
-        <Text style={nextStopStyles.buttonText}>Getting me off this bus</Text>
+        <Text style={nextStopStyles.buttonText}>Get me off this busss!!</Text>
       </TouchableOpacity>
     </View>
   );
@@ -112,15 +142,13 @@ function Support() {
   );
 }
 
-function Map() {
-  return <View style={styles.container}></View>;
-}
-
 function Body({ children }) {
   return <View style={styles.body}>{children}</View>;
 }
 
 function RouteSummary() {
+  const progress = 50; 
+  
   return (
     <View style={routeSummaryStyles.container}>
       <View style={styles.currentJourneyContainer}>
@@ -135,7 +163,16 @@ function RouteSummary() {
           <Text style={styles.locationText}>Current Location</Text>
           <Text style={styles.locationText}>Next Location</Text>
         </View>
-        <View style={styles.journeyProgressBar}></View>
+        {/* Changed: Replaced simple progress bar with new segmented version */}
+        <View style={styles.progressBarContainer}>
+          <View style={[styles.completedProgress, { width: `${progress}%` }]} />
+          <View style={[styles.remainingProgress, { width: `${100 - progress}%` }]} />
+          <View style={[styles.busIconContainer, { left: `${progress}%` }]}>
+            <View style={styles.busIcon}>
+              <FontAwesomeIcon icon={faBus} color="#0e766f" size={12} />
+            </View>
+          </View>
+        </View>
         <View style={styles.locationHeader}>
           <Text style={styles.locationText}>2 stops passed</Text>
           <Text style={styles.locationText}>2 stops remaining</Text>
@@ -152,7 +189,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   body: {
+    flex: 1,
     padding: 20,
+    gap: 10,
   },
   currentJourneyContainer: {
     flexDirection: "row",
@@ -161,13 +200,7 @@ const styles = StyleSheet.create({
   journeyTextContainer: {
     marginLeft: 10,
   },
-  journeyProgressBar: {
-    height: 15,
-    width: "100%",
-    backgroundColor: "#ffffff",
-    borderRadius: 15,
-    marginVertical: 10,
-  },
+  // Changed: Removed old journeyProgressBar style
   journeySubText: {
     color: "#ffffff",
   },
@@ -184,10 +217,51 @@ const styles = StyleSheet.create({
   locationText: {
     color: "#fff",
   },
+  mapContainer: { 
+    flex: 1 
+  },
   mapView: {
-    marginTop: 10,
+    flex: 1,
     width: "100%",
-    height: "45%",
+  },
+
+  progressBarContainer: {
+    height: 12,
+    width: "100%",
+    borderRadius: 15,
+    marginVertical: 10,
+    flexDirection: 'row',
+  
+  
+  },
+  completedProgress: {
+    height: '100%',
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 15,
+    borderBottomLeftRadius: 15,
+  },
+  remainingProgress: {
+    height: '100%',
+    backgroundColor: "#115f58",
+    borderTopRightRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+  busIconContainer: {
+    position: 'absolute',
+    top: -8,
+    width: 30,
+    height: 30,
+    marginLeft: -15, // Center the icon on the progress point
+  },
+  busIcon: {
+    backgroundColor: "#FFFFFF",
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 3,
+    borderColor: "#0e766f",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
@@ -284,7 +358,7 @@ const supportStyles = StyleSheet.create({
   forgotButton: {
     flexDirection: "row",
     width: "48%",
-    backgroundColor: "#FF00FF",
+    backgroundColor: "#7C26A0",
     padding: 10,
     borderRadius: 10,
     gap: 5,
